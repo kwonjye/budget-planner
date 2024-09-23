d34ff9125a741fff4ebf19dab0f2ec454ccb331d
KWONJYE <sheisdanmi@gmail.com>
2024-09-23 21:12:19 +0900

    프로젝트 초기 설정

---
+++ src/test/java/kwonjye/budget_planner/BudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package kwonjye.budget_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
