251b0ff1d62c4b007b5a3459a874ae17bdf80b41
KWONJYE <sheisdanmi@gmail.com>
2024-10-06 13:14:07 +0900

    fix - 자산 음수 입력 가능

--- src/main/java/jye/budget/form/AssetForm.java
+++ src/main/java/jye/budget/form/AssetForm.java
@@ -1,6 +1,5 @@
 package jye.budget.form;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jye.budget.entity.Asset;
 import lombok.AllArgsConstructor;
@@ -17,7 +16,6 @@ public class AssetForm {
     @NotBlank
     private String assetName;
 
-    @Min(value = 0, message = "초기 금액은 0 이상이어야 합니다.")
     private int initialAmount;
 
     private boolean isAllocated;
